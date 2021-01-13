a7844230ac99a78c2ff44fa03c1b2c33193abdd8
Alex Wang <alw245@pitt.edu>
2021-01-13 14:41:38 -0500

    fixed styles

--- react-ui/src/Components/Footer.js
+++ react-ui/src/Components/Footer.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+const Footer2 = ({ data }) => {
+  const networks = data.social.map(n =>
+    <li key={n.name}><a href={n.url}><i className={n.className}></i></a></li>
+  )
+  return (
+    <footer>
+
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">
+            {networks}
+          </ul>
+
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+      </div>
+    </footer>
+  );
+}
 
 class Footer extends Component {
   render() {
 
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
     }
@@ -12,21 +31,21 @@ class Footer extends Component {
     return (
       <footer>
 
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
               {networks}
-           </ul>
+            </ul>
 
-     {/*     <ul className="copyright">
+            {/*     <ul className="copyright">
               <li>&copy; Copyright 2017 Nordic Giant</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>*/}   
+           </ul>*/}
 
+          </div>
+          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+      </footer>
     );
   }
 }
