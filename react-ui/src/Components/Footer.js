a0e03513d81c2ef7d9f3e4a1e69e2fcd5eb290fd
Alex Wang <alw245@pitt.edu>
2021-01-13 18:23:27 -0500

    readme

--- react-ui/src/Components/Footer.js
+++ react-ui/src/Components/Footer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Footer = ({ data }) => {
   const networks = data.social.map(n =>
--- react-ui/src/Components/Header.js
+++ react-ui/src/Components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Particles from './Proton/Particles'
 
 import { getSOTD } from './services/serverServices'
