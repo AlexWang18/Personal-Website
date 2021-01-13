d6a1c87a13cc17ec42c8897138567ca45ccd7ca1
Alex Wang <alw245@pitt.edu>
2021-01-13 14:25:32 -0500

    added spotify picture

--- react-ui/src/Components/Header.js
+++ react-ui/src/Components/Header.js
@@ -5,14 +5,14 @@ import { getSpotify } from '../services/serverServices'
 
 const Header = ({ data }) => {
    const [song, setSong] = useState(null)
-   
+
    useEffect(() => {
-      async function fetch(){
+      async function fetch() {
          const songOTD = await getSpotify()
          setSong(songOTD)
       }
       fetch()
-   },[])
+   }, [])
 
    var github = data.github;
    var name = data.name;
@@ -21,44 +21,44 @@ const Header = ({ data }) => {
       return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
    })
 
-   if(song === null){
+   if (song === null) {
       console.log(song)
       return <h1>Loading...</h1>
    }
    console.log(song)
    return (
-         <header id="home">
-            <nav id="nav-wrap">
-               <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-               <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+      <header id="home">
+         <nav id="nav-wrap">
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+            <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-               <ul id="nav" className="nav">
-                  <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-                  <li><a className="smoothscroll" href="#about">About</a></li>
-                  <li><a className="smoothscroll" href="#resume">Resume</a></li>
-                  <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-                  <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            <ul id="nav" className="nav">
+               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+               <li><a className="smoothscroll" href="#about">About</a></li>
+               <li><a className="smoothscroll" href="#resume">Resume</a></li>
+               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+               <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            </ul>
+         </nav>
+
+         <div className="row banner">
+
+            <div className="banner-text">
+               <h1 className="responsive-headline">{name}</h1>
+               <h3>{description}.</h3>
+               <hr />
+               <ul className="social">
+                  <li><a href={song.preview} className="button btn spotify-btn"><i className="fa fa-book"></i>Song of the day: {song.name} - {song.artist}</a></li>
+                  <li><a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a></li>
                </ul>
-            </nav>
-
-            <div className="row banner">
-
-               <div className="banner-text">
-                  <h1 className="responsive-headline">{name}</h1>
-                  <h3>{description}.</h3>
-                  <hr />
-                  <ul className="social">
-                     <a href={song.url} className="button btn spotify-btn"><i className="fa fa-book"></i>Song of the day</a>
-                     <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
-                  </ul>
-               </div>
             </div>
+         </div>
 
-            <p className="scrolldown">
-               <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-            </p>
-            <Particles bg={true} />
-         </header>
+         <p className="scrolldown">
+            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+         </p>
+         <Particles bg={true} />
+      </header>
    )
 }
 
