958888edb60286a662a58870c58e3d5ee098ccaa
Alex Wang <alw245@pitt.edu>
2021-01-15 10:39:44 -0500

    removed homepage from frontend

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 3002
 let MONGODB_URI = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test'){
