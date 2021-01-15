2506b5e37d52cc3e51d34b2ac1b236bb1a00daf2
Alex Wang <alw245@pitt.edu>
2021-01-14 20:05:14 -0500

    use npm start

--- server/app.js
+++ server/app.js
@@ -8,10 +8,11 @@ const cors = require('cors')
 const spotifyRouter = require('./controllers/spotify')
 
 const app = express()
+console.log('mode: ',process.env.NODE_ENV)
 
 const mongoose = require('mongoose')
-console.log(process.env.NODE_ENV)
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }) //undefined in prod mode
     .then(succ => {
         console.log('connected to MongoDB')
     })
